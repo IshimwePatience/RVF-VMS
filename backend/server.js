dc9015e08eed8919817e593b4589fa1fcda65156
IshimwePatience <ishimweaugstin12@gmail.com>
2026-07-16 11:08:23 +0200

    Add RAB logo image

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const io = new Server(server, {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 // Routes
 
